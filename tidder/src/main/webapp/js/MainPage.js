600047980239794bf91885eb6d5d5eb1c46c2e64
Krzysztof Abram <krzysztofabram95@gmail.com>
2018-04-02 15:28:08 +0200

    Pagination finish

--- tidder/src/main/webapp/js/MainPage.js
+++ tidder/src/main/webapp/js/MainPage.js
@@ -2,41 +2,6 @@
     'use strict';  
     var data;
     var myApp = angular.module('Tidder',['ui.bootstrap']);
-
-    myApp.controller('MainPageController', ['$scope','$http', function($scope,$http) {
-      $scope.double = function(value) { return value * 2; };
-      
-        function GetAllPosts () {
-          $http({method: 'GET', url: 'http://localhost:8080/tidder/GetAllPosts.json'})
-          .then( function(response) {
-        	  
-        	  $scope.posts = response.data;
-          
-          }, function errorCallback(response) {
-        	  
-        	  	alert('ERROR !! ' + response.status);
-        		})
-          
-        }
- 
-        $scope.mydata='null';
-        var config = {
-        		 params: data,
-        		 headers : {'Accept' : 'application/json'}
-        		};
- 
-        $http.get('http://localhost:8080/tidder/webapi/post/all', config).then(function(data) {
-        	
-        	console.log(data);
-        	$scope.mydata=data.data;
-        	
-        }, function() {
-        	alert('failure');
-        });
-          GetAllPosts();
-        
-    
-    }]);
     
     myApp.controller('PaginationMyController', function($scope,$http) {
         var config = {
@@ -50,7 +15,6 @@
         
         function setCurrentPage(page) {
         	$scope.currentPage = page;
-        	alert('start' + currentPage-1*itemsOnPageAmount + ' end ' + currentPage*itemsOnPageAmount);
         }
         
         function getAllPosts() {
